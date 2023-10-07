4a0de7b40dbc6a59acd706a3af108e1484cc0d4b
connorjohnson6 <70149458+connorjohnson6@users.noreply.github.com>
2023-10-07 18:29:29 -0400

    Made a memory exceeder checker

--- source/host/memoryAccessor.ts
+++ source/host/memoryAccessor.ts
@@ -3,6 +3,9 @@ module TSOS {
     
     export class MemoryAccessor {
 
+        private writeCount: number = 0;
+        private maxWriteCount: number = 0xFF; 
+
         constructor() {}
 
         public init(){}
@@ -12,13 +15,33 @@ module TSOS {
             return _Memory[address];
         }
 
+
         // Write a byte to a specific address in memory.
         public write(address: number, value: number): void {
+
             _Memory[address] = value;
     
             TSOS.Control.updateMemory(address, value);
+        
+            //looking for potention infite loops due to it exceeding memory limit
+            if (_CPU.Yreg > 0XFF) {
+                // Terminate process or take other appropriate action
+                _CPU.isExecuting = false;
+                _CPU.init();  // re-initialize or clear CPU state
+
+                if (_CPU.currentPCB) {
+                    _CPU.currentPCB.state = "Terminated";
+                    TSOS.Control.updatePCBs();
+                    _StdOut.putText(`Process ${_CPU.currentPCB.pid} has been manually terminated`);
+                
+                    
+                }
+
+                Kernel.krnTrapError("Potential infinite loop detected: too many write operations");
+
+            }
+            
+            
         }
-        
-        
     }
 }
